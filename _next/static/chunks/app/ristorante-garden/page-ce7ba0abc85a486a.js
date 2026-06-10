28d940141cda8709b3f19d8ce81d242e6c398019
Samuele Simonini <samuelesimonini@192.168.1.104>
2026-06-11 01:38:23 +0200

    Fix Alpina asset paths for GitHub Pages

--- _next/static/chunks/app/ristorante-garden/page-ce7ba0abc85a486a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[545,181,56,139,597,800,928,54,213,799,517,334,310,446,557,159,39,241,57,931],{422:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,878,971,117,744],function(){return e(e.s=422)}),_N_E=e.O()}]);
