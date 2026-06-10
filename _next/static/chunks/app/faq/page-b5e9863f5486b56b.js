d39916ecfb657c69b59afbae8a0fffd004e0cd42
Samuele Simonini <samuelesimonini@192.168.1.104>
2026-06-11 01:34:42 +0200

    Add Alpina ElevenLabs concierge widget

---
+++ _next/static/chunks/app/faq/page-b5e9863f5486b56b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[799,181,56,139,597,800,928,54,213,517,334,310,446,557,159,39,545,241,57,931],{422:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,878,971,117,744],function(){return e(e.s=422)}),_N_E=e.O()}]);
